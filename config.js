776e57ca4c2f74ee4ac5a0e19a208d0c871c0ee3
FRA3A <annacodit@gmail.com>
2026-01-06 04:11:49 +0400

    removing config.js from tracking

--- config.js
+++
@@ -1,5 +0,0 @@
-// --- КОНФИГУРАЦИЯ ---
-const MY_CHANNEL = 'akane_iwawwa';
-const BOTS = ['streamelements', 'jeetbot', 'potatbotat', 'frostytoolsdotcom', 'moobot', 'nightbot', 'wizebot'];
-const COOL_USERS = ['FAROSTG', 'akane_iwawwa', 'tischhhh', 'annacodit'];
-const MAX_LOG_LINES = 50;
