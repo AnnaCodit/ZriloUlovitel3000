6a58d19ab1accd880b5e52df5b6e731b0ff439fc
FRA3A <annacodit@gmail.com>
2026-01-06 04:14:01 +0400

    123

--- config-sample.js
+++ config-sample.js
@@ -1,4 +1,5 @@
 // --- КОНФИГУРАЦИЯ ---
+// заполните название канала и переименуйте этот файл в config.js для начала работы
 const MY_CHANNEL = ''; // название канала
 const BOTS = ['streamelements', 'jeetbot', 'potatbotat', 'frostytoolsdotcom', 'moobot', 'nightbot', 'wizebot'];
 const COOL_USERS = []; // подсвечивать тех кто уже не новенький, но хочется их замечать среди луркеров
